b706ecf21dfad94d086f7e09c248a78024fa3d2f
Syeri500 <syeri.TX@gmail.com>
2023-06-15 14:25:06 +0800

    test

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,14 +1,27 @@
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model, ModelStatic } from "sequelize";
 import sequelize from "./sequelize";
+import { AssociatedModels } from ".";
 
 interface UserAttributes {
 	id: number;
 	email: string;
 	password: string;
 }
+
 class User extends Model<UserAttributes> {
 	public id!: number;
 	public email!: string;
+
+	// associate method to establish relationships with other models
+	/* The Following is an example:
+	
+	static associate(models: AssociatedModels): void {
+		User.hasMany(models.Note, {
+			foreignKey: "userId",
+			onDelete: "cascade"
+		});
+	};
+	*/
 }
 
 User.init({
