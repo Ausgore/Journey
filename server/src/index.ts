b377071478e5e2157182cc8dcd5e36694f984373
Syeri500 <134137512+Syeri500@users.noreply.github.com>
2023-06-15 14:18:55 +0800

    Update index.ts to enable json data

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 sequelize.sync({ alter: true }).then(function() {
 	app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
